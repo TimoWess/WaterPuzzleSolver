b4023bd56f51065790e8fd4fb84bf301d2ad932b
Timo Wesselmann <timo.wesselmann@gmail.com>
2022-07-23 19:58:07 +0200

    Working version

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,11 @@
 module Lib where
 
+import Control.Monad
 import Data.Char
+import Foreign (moveBytes)
+import GHC.Base (VecElem(Int16ElemRep))
+import System.IO
+import System.Random
 
 data Color
     = Grey
@@ -21,6 +26,10 @@ type Board = [Tube]
 
 type Tube = ([Color], Int)
 
+type Move = (Int, Int)
+
+maxFails = 10000
+
 isFull :: Tube -> Bool
 isFull tube@(colors, index) = length colors == 4
 
@@ -33,8 +42,10 @@ pourSize tube@(colors, index) = length (takeWhile (== head colors) colors)
 
 canPour :: Tube -> Tube -> Bool
 canPour t1@(colors1, i1) t2@(colors2, i2)
+    | i1 == i2 = False
     | isFull t2 = False
     | null colors1 = False
+    | pourSize t1 == length colors1 && null colors2 = False
     | null colors2 = True
     | let neededSpace = pourSize t1
           space = freeSpace t2
@@ -66,39 +77,133 @@ replaceTubes t1 t2 nt1 nt2 currentTube
     | currentTube == t2 = nt2
     | otherwise = currentTube
 
-getBoard board = do
-    input <- getLine
-    if null input
-        then putStrLn "done"
-        else do
-            let newColor = getColor input
-                newBoard = board ++ [newColor]
-            print newBoard
-            getBoard newBoard
+getBoard :: IO Board
+getBoard = fn' []
+  where
+    fn' board = do
+        putStr "Add a new tube? [y/n] "
+        hFlush stdout
+        input <- getLine
+        if null input || input == "n"
+            then return (reverse board)
+            else do
+                newTube <- getTube
+                let newBoard = (newTube, length board) : board
+                print newBoard
+                fn' newBoard
 
+getTube :: IO [Color]
+getTube = fn' []
+  where
+    fn' colors = do
+        putStr "Add a color: "
+        hFlush stdout
+        input <- getLine
+        if null input
+            then return colors
+            else do
+                let newColor = getColor input
+                case newColor of
+                    Just val -> do
+                        let newColors = colors ++ [val]
+                        print newColors
+                        fn' newColors
+                    Nothing -> do
+                        putStrLn "Invalid color. Try again!"
+                        fn' colors
+
+getColor :: String -> Maybe Color
 getColor x =
     case map toLower x of
-        "red" -> Red
-        "grey" -> Grey
-        "lightblue" -> LightBlue
-        "yellow" -> Yellow
-        "brown" -> Brown
-        "orange" -> Orange
-        "darkblue" -> DarkBlue
-        "pink" -> Pink
-        "purple" -> Purple
-        "lightgreen" -> LightGreen
-        "green" -> Green
-        "darkGreen" -> DarkGreen
-        _ -> undefined
+        "red" -> Just Red
+        "grey" -> Just Grey
+        "lightblue" -> Just LightBlue
+        "yellow" -> Just Yellow
+        "brown" -> Just Brown
+        "orange" -> Just Orange
+        "darkblue" -> Just DarkBlue
+        "pink" -> Just Pink
+        "purple" -> Just Purple
+        "lightgreen" -> Just LightGreen
+        "green" -> Just Green
+        "darkgreen" -> Just DarkGreen
+        _ -> Nothing
 
-getTube =
-    let fn' colors = do
+leftPad :: String -> Int -> Char -> String
+leftPad str num chr = replicate (num - length str) chr ++ str
+
+printBoard :: Board -> IO ()
+printBoard board =
+    forM_ board $ \i -> do
+        let (colors, index) = i
+            maxLength = maximum [length $ show x | (y, x) <- board]
+        putStrLn $ leftPad (show index) maxLength '0' ++ ": " ++ show colors
+
+play :: Board -> IO ()
+play board = do
+    putStr
+        "Move, add a tube, print board, enable autoplay, clear board or quit? [m/a/p/auto/c/q] "
+    hFlush stdout
+    input <- getLine
+    case map toLower input of
+        "p" -> do
+            printBoard board
+            play board
+        "m" -> do
+            putStr "Select pouring tube: "
+            hFlush stdout
             input <- getLine
-            if null input
-                then colors
-                else do
-                    newColor <- getColor input
-                    let newColors = colors ++ [newColor]
-                    fn' newColors
-     in fn' []
+            let i1 = read input :: Int
+            putStr "Select revieving tube: "
+            hFlush stdout
+            input <- getLine
+            let i2 = read input :: Int
+                (success, newBoard) = pour board i1 i2
+            print newBoard
+            play newBoard
+        "a" -> do
+            newTube <- getTube
+            let newBoard = reverse ((newTube, length board) : reverse board)
+            play newBoard
+        "c" -> do
+            play []
+        "q" -> do
+            putStrLn "Good game!"
+            return ()
+        "auto" -> do
+            (newBoard, moves, moveList) <- autoPlay board
+            putStrLn $ show moves ++ " moves needed!"
+            let options = do
+                    putStr "Show move list, continue or new game? [s/c/n] "
+                    hFlush stdout
+                    input <- getLine
+                    case map toLower input of
+                        "s" -> do
+                            printBoard board
+                            print moveList
+                            printBoard newBoard
+                            play newBoard
+                        "c" -> play newBoard
+                        "n" -> play board
+                        _ -> do
+                            putStrLn "Invalid input!"
+                            options
+            options
+        _ -> do
+            putStrLn "Invalid input. Try again!"
+            play board
+
+autoPlay :: Board -> IO (Board, Integer, [(Int, Int)])
+autoPlay board = fn' board 0 0 []
+  where
+    fn' :: Board -> Integer -> Integer -> [Move] -> IO (Board, Integer, [Move])
+    fn' board moves fails moveList = do
+        if fails >= maxFails
+            then return (board, moves, moveList)
+            else do
+                r1 <- randomRIO (0, length board - 1)
+                r2 <- randomRIO (0, length board - 1)
+                let (success, newBoard) = pour board r1 r2
+                if success
+                    then fn' newBoard (moves + 1) 0 ((r1, r2) : moveList)
+                    else fn' board moves (fails + 1) moveList
